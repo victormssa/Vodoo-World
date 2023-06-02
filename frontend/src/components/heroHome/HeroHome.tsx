fc66613b88f94ac57f0e103aece4fc54f360c7a9
Victor Alves <victormssalves@outlook.com>
2023-06-01 22:25:20 -0300

    Update HeroHome.tsx

--- frontend/src/components/heroHome/HeroHome.tsx
+++ frontend/src/components/heroHome/HeroHome.tsx
@@ -10,8 +10,8 @@ function HeroHome() {
             `url(${photo})`,}}>
         <div className="flex items-center justify-center w-full h-full bg-gray-900/40">
             <div className="text-center">
-            <img className="w-[24rem] h-auto  ml-[5rem]" src={logoWhite} alt="Logo da Vodoo World" />
-                <h1 className="text-3xl font-semibold text-white lg:text-4xl sm:text-base w-[34rem]">O cuidado e amor incondicionais que seu pet merece!</h1>
+            <img className="lg:w-[24rem] w-[10rem] h-auto  ml-[5rem]" src={logoWhite} alt="Logo da Vodoo World" />
+                <h1 className="text-3xl font-semibold text-white lg:text-4xl sm:text-base w-[20rem] lg:w-[34rem]">O cuidado e amor incondicionais que seu pet merece!</h1>
             </div>
         </div>
     </div>
