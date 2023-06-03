f00b84217c0347203fc3c051d67e583dfd2279be
Victor Alves <victormssalves@outlook.com>
2023-06-03 19:45:03 -0300

    ESTÁ VIVO!

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getOnline(): string {
+    return 'O Back-End está online!';
   }
 }
