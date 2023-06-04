9887be1da66d59d7365ca8bd896864a21a4fedab
Victor Alves <victormssalves@outlook.com>
2023-06-04 14:32:20 -0300

    Estilização feita

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, Res } from '@nestjs/common';
 import { Response } from 'express';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
   getOnline(@Res() res: Response): void {
-    res.sendFile('./../public/index.html', { root: 'public' });
+    const filePath = path.join(__dirname, '..', 'public', 'index.html');
+    res.sendFile(filePath);
   }
 }
