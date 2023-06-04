cf2e57adde693d6a6c6ed304ad8def8f1d8e5b58
Victor Alves <victormssalves@outlook.com>
2023-06-04 14:16:59 -0300

    Página de index.html no backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Injectable()
 export class AppService {
-  getOnline(): string {
-    return "<h1>Vodoo World - Pet Shop</h1><br><h2>Status: 200 OK | O Back-End está online e funcional.</h2><br> <h2>Rotas:</h2> <a href='http://localhost:3000/users'><h2>Usuários</h2></a>";
+  getOnline(@Res() res: Response): void {
+    res.sendFile('index.html', { root: 'public' });
   }
 }
