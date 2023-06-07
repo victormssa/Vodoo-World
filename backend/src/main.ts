61ce8bfc0175bde23ae6743cad4e90f5d09a9b32
Victor Alves <victormssalves@outlook.com>
2023-06-06 23:12:23 -0300

    Inserindo o cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   try {
     const port = 3000; // Porta do servidor
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    // Configuração do CORS
+    app.use(
+      cors({
+        origin: ['http://localhost:5173', 'https://vodooworld.vercel.app'],
+        credentials: true,
+      }),
+    );
 
     // Inicialização do servidor
     await app.listen(port);
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,14 @@
 import Header from '../components/header/Header';
 import LoginForm from '../components/loginForm/LoginForm';
 import Footer from '../components/footer/Footer';
+import usePageTitle from '../hooks/UsePageTitle';
 
 const Login: React.FC = () => {
   return (
     <>
+      {usePageTitle("Login | Vodoo World")}
         <Header/>
-        <main className='mt-12 py-20'><LoginForm/></main>
+        <main className='mt-12 mb-12 py-[3.75rem]'><LoginForm/></main>
         <Footer/>
     </>
   );
