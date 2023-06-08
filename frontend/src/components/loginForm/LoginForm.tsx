035272c5dccb8c5a5de9d03fe783c523ce795aff
Victor Alves <victormssalves@outlook.com>
2023-06-07 23:25:51 -0300

    Update LoginForm.tsx

--- frontend/src/components/loginForm/LoginForm.tsx
+++ frontend/src/components/loginForm/LoginForm.tsx
@@ -66,7 +66,7 @@ const LoginForm: React.FC = () => {
     e.preventDefault();
     const newItem: Credentials = { username: username, password };
     try {
-      const url = `http://api-vodoo-world.vercel.app/auth/login`;
+      const url = `https://api-vodoo-world.vercel.app/auth/login`;
   
       const response = await axios.post(url, newItem);
       const data = response.data;
